9dd3ea26194b08f736d7dcf54350615d077fb03d
Mineth Weerasinghe <minethweerasinghe@gmail.com>
2025-01-30 12:57:38 +0530

    setting up asgardeo

---
+++ src/auth.ts
@@ -0,0 +1,8 @@
+import NextAuth from "next-auth"
+import Asgardeo from "next-auth/providers/asgardeo"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Asgardeo({
+    issuer: process.env.AUTH_ASGARDEO_ISSUER
+  })],
+})
