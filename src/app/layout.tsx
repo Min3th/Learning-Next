03419e7fd15b64e93a37c5cb5803378b8faea83d
Min3th <minethweerasinghe@gmail.com>
2025-02-02 01:17:25 +0530

    positional issues related to sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="">
-        <Navbar />
-        <MiniDrawer />
+      <body className=" ">
+        <div className="">
+          <Navbar />
+        </div>
+
+        <div className="mt-10">
+          <MiniDrawer />
+        </div>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </AppRouterCacheProvider>
