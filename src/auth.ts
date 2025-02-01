812c907356c7eaa131989def2dd091d13cfa4f25
Min3th <minethweerasinghe@gmail.com>
2025-02-01 23:23:37 +0530

    fixed redirect url issue regards to asgardeo

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,17 @@
-import NextAuth from "next-auth"
-import Asgardeo from "next-auth/providers/asgardeo"
+import NextAuth from "next-auth";
+import Asgardeo from "next-auth/providers/asgardeo";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Asgardeo({
-    issuer: process.env.AUTH_ASGARDEO_ISSUER
-  })],
-})
+  providers: [
+    Asgardeo({
+      issuer: process.env.AUTH_ASGARDEO_ISSUER,
+      clientId: process.env.ASGARDEO_CLIENT_ID,
+      clientSecret: process.env.ASGARDEO_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl; // Redirects to home page after login
+    },
+  },
+});
